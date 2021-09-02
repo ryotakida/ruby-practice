9d8449ee17fd666614ec08ad4178b0efdcac837b
RyotaKida <uulee5521@gmail.com>
2021-09-02 10:16:36 +0000

    [finish]2-6

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '2c0a00bfc9802078b280c4b6d3634108a57066685001852dee2c29533a2b07a15259624ad14ad1ea3abff8b9a6ea388dd4ffb34e654583591155916df60944da'
