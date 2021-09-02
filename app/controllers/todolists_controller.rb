41c8583fba8826594ea63e97ff80f27a69e7977a
RyotaKida <uulee5521@gmail.com>
2021-09-02 11:12:17 +0000

    [finish]2-9

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -23,7 +23,7 @@ class TodolistsController < ApplicationController
   end
 
   def destroy
-        list = List.find(params[:id])  # データ（レコード）を1件取得
+    list = List.find(params[:id])  # データ（レコード）を1件取得
     list.destroy  # データ（レコード）を削除
     redirect_to todolists_path  # 投稿一覧画面へリダイレクト
   end
